04d4d0e5feecdd635442e79f82f19f7c5bb78060
Timothy Lor <43763078+tulXoro@users.noreply.github.com>
2018-12-10 14:30:47 -0800

    Update Spawner.java

--- src/game/Spawner.java
+++ src/game/Spawner.java
@@ -9,20 +9,22 @@ public class Spawner {
 	private Game game;
 	private int cnt;
 	public Spawner(Handler h, Player p, Game game) {
-		
+		//variable setting
 		this.h = h;
 		this.p = p;
 		this.game = game;
-		cnt = 500;
+		cnt = 1000;
 	}
 	
 	public void tick(){
+		//Spawns new enemies at certain intervals
 		if(cnt%100==0) h.addEnemy(new BasicEnemy(Game.WIDTH-26,r.nextInt(Game.HEIGHT), 10, 1, p, game));
 		if(cnt%5 == 0) h.addEnemy(new FastEnemy(r.nextInt(10),r.nextInt(Game.HEIGHT-24), 1, 1, game));
 		
 		//when counter reaches 0, counter resets
 		if(cnt<=0) cnt=1000;
 		cnt--; //constantly remove counter
+		
 		
 		for(int i = 0; i<h.list.size(); i++) {
 			EnemyB temp = h.list.get(i);
